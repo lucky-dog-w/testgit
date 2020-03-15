6d40e8d6ca525ef6db8159f61701ac5a6a69ed88
lucky-dog-w <1069849274@qq.com>
2020-03-15 16:38:40 +0800

    做了修改

--- src/main/java/com/lucky/dog/HelloGit.java
+++ src/main/java/com/lucky/dog/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
 
     public static void main(String[] args) {
         System.out.println(1122334455);
-        System.out.println("测试git,推送到远程仓库");
-         System.out.println(222222);
+         System.out.println(112233);
+        System.out.println("我做了修改先不提交");
     }
 }
