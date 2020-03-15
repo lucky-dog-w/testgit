ec43f109cbf0280bd9b6a494f36eea1ea1bd2990
lucky-dog-w <1069849274@qq.com>
2020-03-15 18:40:20 +0800

    解决完冲突

--- src/main/java/com/lucky/dog/HelloGit.java
+++ src/main/java/com/lucky/dog/HelloGit.java
@@ -3,7 +3,6 @@ package com.lucky.dog;
 public class HelloGit {
 
     public static void main(String[] args) {
-        System.out.println("我做了修改先不提交");
-        System.out.println("a添加了a");
+        System.out.println("我做了修改冲突了");
     }
 }
--- src/main/java/com/lucky/dog/TestGit.java
+++ src/main/java/com/lucky/dog/TestGit.java
@@ -5,5 +5,6 @@ public class TestGit {
     public static void main(String[] args) {
         System.out.println(111);
         System.out.println(222);
+        System.out.println(333);
     }
 }
