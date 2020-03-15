25987663b5c581032ef7458735621eac183622d5
lucky-dog-w <1069849274@qq.com>
2020-03-15 22:06:47 +0800

    我是test

--- src/main/java/com/lucky/dog/test2.java
+++ src/main/java/com/lucky/dog/test2.java
@@ -2,6 +2,6 @@ package com.lucky.dog;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("我合并到主分支 把主分支给覆盖了");
+        System.out.println("我是test");
     }
 }
