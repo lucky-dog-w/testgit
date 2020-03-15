94dcfd44a4441b38ab7d2f62ca83f3b4777b4990
lucky-dog-w <1069849274@qq.com>
2020-03-15 21:50:38 +0800

    Merge branch 'master' of https://github.com/lucky-dog-w/testgit
    # Conflicts:
    #	src/main/java/com/lucky/dog/test2.java

--- src/main/java/com/lucky/dog/TestGit.java
+++ src/main/java/com/lucky/dog/TestGit.java
@@ -3,8 +3,6 @@ package com.lucky.dog;
 public class TestGit {
 
     public static void main(String[] args) {
-        System.out.println(111);
-        System.out.println(222);
-        System.out.println(333);
+        System.out.println("主分支修改完了 提交之后我要切换到dev");
     }
 }
