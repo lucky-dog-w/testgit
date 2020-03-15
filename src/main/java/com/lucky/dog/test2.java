cd272cfdad3c966ac59107dac0f81ac501b19dca
lucky-dog-w <1069849274@qq.com>
2020-03-15 18:49:50 +0800

    22

--- src/main/java/com/lucky/dog/test2.java
+++ src/main/java/com/lucky/dog/test2.java
@@ -2,7 +2,9 @@ package com.lucky.dog;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("bbb");
-        System.out.println("添加了b");
+        System.out.println("aa");
+        System.out.println("添加了aa");
+        System.out.println(11);
+        System.out.println(22);
     }
 }
