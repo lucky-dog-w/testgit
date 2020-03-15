e0cb72b14bfe11155e84aa56e35a2d6c1905193a
lucky-dog-w <1069849274@qq.com>
2020-03-15 16:10:10 +0800

    112233

--- src/main/java/com/lucky/dog/HelloGit.java
+++ src/main/java/com/lucky/dog/HelloGit.java
@@ -3,7 +3,7 @@ package com.lucky.dog;
 public class HelloGit {
 
     public static void main(String[] args) {
-        System.out.println(1111222233333);
+        System.out.println(112233);
         System.out.println("测试git,推送到远程仓库");
     }
 }
