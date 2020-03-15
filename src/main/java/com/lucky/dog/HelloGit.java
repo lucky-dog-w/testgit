efbdf6abee68f927605bd3a28d9bca29bf54d436
lucky-dog-w <1069849274@qq.com>
2020-03-15 21:44:52 +0800

    主分支标识

--- src/main/java/com/lucky/dog/HelloGit.java
+++ src/main/java/com/lucky/dog/HelloGit.java
@@ -3,7 +3,6 @@ package com.lucky.dog;
 public class HelloGit {
 
     public static void main(String[] args) {
-        System.out.println("这个是分支dev");
-        System.out.println("开发");
+        System.out.println("这个是主分支：master");
     }
 }
--- src/main/java/com/lucky/dog/test2.java
+++ src/main/java/com/lucky/dog/test2.java
@@ -2,9 +2,7 @@ package com.lucky.dog;
 
 public class test2 {
     public static void main(String[] args) {
-        System.out.println("aa");
-        System.out.println("添加了aa");
-        System.out.println(11);
-        System.out.println(22);
+        System.out.println("主分支");
+
     }
 }
