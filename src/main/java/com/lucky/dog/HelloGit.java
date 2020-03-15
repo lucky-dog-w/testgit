abec0aaaa537c51544334f5fb615422df005e838
lucky-dog-w <1069849274@qq.com>
2020-03-15 18:31:50 +0800

    tianjia

--- src/main/java/com/lucky/dog/HelloGit.java
+++ src/main/java/com/lucky/dog/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
 
     public static void main(String[] args) {
         System.out.println("我做了修改先不提交");
+        System.out.println("a添加了a");
     }
 }
--- src/main/java/com/lucky/dog/test2.java
+++ src/main/java/com/lucky/dog/test2.java
@@ -3,5 +3,6 @@ package com.lucky.dog;
 public class test2 {
     public static void main(String[] args) {
         System.out.println(222);
+        System.out.println("添加了b");
     }
 }
