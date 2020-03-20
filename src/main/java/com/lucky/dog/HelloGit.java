6e536841d04127afffb8958aa8c049e03b837074
lucky-dog-w <1069849274@qq.com>
2020-03-20 22:03:44 +0800

    111

--- src/main/java/com/lucky/dog/HelloGit.java
+++ src/main/java/com/lucky/dog/HelloGit.java
@@ -4,7 +4,6 @@ public class HelloGit {
 
     public static void main(String[] args) {
         System.out.println("我是b主分支");
-System.out.println("我是线上增加了");
         System.out.println("这个是主分支：master");
         System.out.println("我是bug2  我只留了个master");
     }
